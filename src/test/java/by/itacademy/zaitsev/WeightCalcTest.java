436284dde8be6c7fdb6dc5747b293d98a121a6b9
Zaitsev <az.pomocnik@gmail.com>
2023-11-13 19:05:20 +0100

    Commit new test with data

--- src/test/java/by/itacademy/zaitsev/WeightCalcTest.java
+++ src/test/java/by/itacademy/zaitsev/WeightCalcTest.java
@@ -30,7 +30,7 @@ public class WeightCalcTest {
         String expected = ("Не указано имя.\nРост должен быть в диапазоне 50-300 см.\nВес должен быть в диапазоне 3-500 кг.\nНе указан пол.");
         Assertions.assertEquals(expected, actual);
 
-        Thread.sleep(40000);
+        Thread.sleep(30000);
         driver.quit();
     }
 
